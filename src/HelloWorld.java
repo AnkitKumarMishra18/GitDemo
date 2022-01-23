c29aa9081394cb91e86cb67c50e91cb794cd9fcc
Nitesh <kumarnit123@gmail.com>
2022-01-23 13:52:09 +0530

    second commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello Test");
+        System.out.println("Hello Test1");
+        System.out.println("Hello Test2");
+
     }
 }
